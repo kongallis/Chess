1333a56988ac35fe9431718ee806011970b2be53
Konstantinos <kongallis@gmail.com>
2020-11-24 19:20:28 +0200

    Made Tiles immutable using guava library

---
+++ src/com.chess.engine.board/Tile.java
@@ -0,0 +1,72 @@
+
+package com.chess.engine.board;
+import com.chess.engine.pieces.Piece;
+import com.google.common.collect.ImmutableMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class Tile {
+
+    protected final int tileCoordinate;
+
+    private static Map<Integer, EmptyTile> EMPTY_TILES = createAllPossibleEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllPossibleEmptyTiles() {
+        final Map<Integer, EmptyTile> emptyTileMap = new HashMap<>();
+        for (int i = 0; i < 64; i++)
+            emptyTileMap.put(i, new EmptyTile(i));
+        // After construct the emptyTileMap, nobody can change or destroy it
+        return ImmutableMap.copyOf(emptyTileMap);
+    }
+
+    // Factory method to create a Tile
+    public static Tile createTile(final int tileCoordinate, final Piece piece) {
+        return piece != null ? new OccupiedTile(tileCoordinate, piece) : EMPTY_TILES.get(tileCoordinate);
+    }
+
+    private Tile(int tileCoordinate) {
+        this.tileCoordinate = tileCoordinate;
+    }
+
+    public abstract boolean isTileOccupied();
+
+    public abstract Piece getPiece();
+
+    public static final class EmptyTile extends Tile{
+
+        EmptyTile(final int coordidante) {
+            super(coordidante);
+        }
+
+
+        @Override
+        public boolean isTileOccupied() {
+            return false;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return null;
+        }
+    }
+
+    public static final class OccupiedTile extends Tile {
+        private final Piece pieceOnTile;
+
+        OccupiedTile(int titleCoordinate, Piece pieceOnTile) {
+            super(titleCoordinate);
+            this.pieceOnTile = pieceOnTile;
+        }
+
+        @Override
+        public boolean isTileOccupied() {
+            return true;
+        }
+
+        @Override
+        public Piece getPiece() {
+            return this.pieceOnTile;
+        }
+    }
+}
